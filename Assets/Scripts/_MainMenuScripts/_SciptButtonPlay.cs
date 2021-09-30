242ad6b1a9e88a23645003e14191a8a893c929c7
richwoud <richwoud@gmail.com>
2021-09-30 18:50:15 +0300

    Добавил новую сцену _MainMenu Настроил немного интерфейса По нажатию кнопки Play загружается сцена игры

---
+++ Assets/Scripts/_MainMenuScripts/_SciptButtonPlay.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class _SciptButtonPlay : MonoBehaviour
+{
+  public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
