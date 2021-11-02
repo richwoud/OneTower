221e401a7c68465e7ee5f9041316d49d510bd646
richwoud <richwoud@gmail.com>
2021-11-03 02:01:52 +0300

    Поменял музыку Добавил ссылки авторов

---
+++ Assets/Scripts/_MainMenuScripts/ButtonsLinks.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsLinks : MonoBehaviour
+{
+    public void OnBtn_Link1()
+    {
+        Application.OpenURL("http://www.abstractionmusic.com/");
+    }
+    public void OnBtn_Link2()
+    {
+        Application.OpenURL("https://www.flaticon.com/authors/freepik");
+    }
+    public void OnBtn_Link3()
+    {
+        Application.OpenURL("https://flat-icons.com/");
+    }
+    public void OnBtn_Link4()
+    {
+        Application.OpenURL("https://www.flaticon.com/authors/nikita-golubev");
+    }
+    public void OnBtn_Link5()
+    {
+        Application.OpenURL("https://www.flaticon.com/authors/indielogy");
+    }
+}
